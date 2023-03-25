0b69e9bc9f087cc6d651b96afddd96c26296a400
Muhammad_Hammad_Shah <hammad4455shah5544@gmail.com>
2023-03-25 20:04:10 +0500

    created divs and give them flex property

--- using_tailwind/src/app/page.tsx
+++ using_tailwind/src/app/page.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function page() {
   return (
-    <div>page</div>
-  )
+    <>
+      <h1 className=" text-lg bg-orange-500"> 786</h1>
+    </>
+  );
 }
